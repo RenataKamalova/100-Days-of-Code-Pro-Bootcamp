3548f6e3be3ec0491c6781c066d3223894e5245c
RenataKamalova <rene.kamalova@gmail.com>
2022-10-03 17:15:01 +0300

    improved 85 lesson part 1 day 8

--- day 8/lesson_85/Caesar_Cipher_Part _1_ Encryption.py
+++ day 8/lesson_85/Caesar_Cipher_Part _1_ Encryption.py
@@ -30,9 +30,10 @@ alphabet = [
     "z",
 ]
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+# print(len(alphabet), alphabet[21])
 
 
 def encrypt(plain_text, shift_amount):
@@ -40,14 +41,34 @@ def encrypt(plain_text, shift_amount):
     for letter in plain_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
-        if new_position <= len(alphabet):
+        if new_position <= len(alphabet) - 1:
             new_letter = alphabet[new_position]
             ciphertext += new_letter
         else:
-            new_letter = alphabet[new_position - position]
+            new_letter = alphabet[new_position - len(alphabet)]
             ciphertext += new_letter
 
     print(f"The encoded text is {ciphertext}")
 
 
-encrypt(text, shift)
+def decrypt(plain_text, shift_amount):
+    deciphered_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        deciphered_text += new_letter
+        # if new_position > 0:
+        #     new_letter = alphabet[new_position]
+        #     deciphered_text += new_letter
+        # else:
+        #     new_letter = alphabet[new_position - 1]
+        #     deciphered_text += new_letter
+
+    print(f"The decoded text is {deciphered_text}")
+
+
+if direction == "encode":
+    encrypt(text, shift)
+elif direction == "decode":
+    decrypt(text, shift)
