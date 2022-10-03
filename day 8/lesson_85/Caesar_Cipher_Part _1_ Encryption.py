934fd9d66c17a9b8a6417ed26eaf52a5a0d31263
RenataKamalova <rene.kamalova@gmail.com>
2022-10-03 18:51:33 +0300

    improved 85 lesson part 1 day 8

--- day 8/lesson_85/Caesar_Cipher_Part _1_ Encryption.py
+++ day 8/lesson_85/Caesar_Cipher_Part _1_ Encryption.py
@@ -1,6 +1,5 @@
 from operator import index
 
-
 alphabet = [
     "a",
     "b",
@@ -29,34 +28,33 @@ alphabet = [
     "y",
     "z",
 ]
-
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def caesar(plain_text, shift_amount, direction):
-    ciphertext = ""
-    deciphered_text = ""
-
-    for letter in plain_text:
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    for letter in start_text:
         position = alphabet.index(letter)
+        if cipher_direction == "decode":
+            shift_amount *= -1
+            new_position = position + shift_amount
+            new_letter = alphabet[new_position]
+            end_text += new_letter
+            shift_amount *= -1
 
-        if direction == "encode":
+        elif cipher_direction == "encode":
             new_position = position + shift_amount
             if new_position <= len(alphabet) - 1:
                 new_letter = alphabet[new_position]
-                ciphertext += new_letter
+                end_text += new_letter
             else:
                 new_letter = alphabet[new_position - len(alphabet)]
-                ciphertext += new_letter
-        print(f"The encoded text is {ciphered_text}")
-        
-        else:            
-            new_position = position - shift_amount
-            new_letter = alphabet[new_position]
-            deciphered_text += new_letter
+                end_text += new_letter
 
-        print(f"The decoded text is {deciphered_text}")
+        # end_text += alphabet[new_position]
+    print(f"The {cipher_direction}d text is {end_text}")
 
-caesar()
+
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
