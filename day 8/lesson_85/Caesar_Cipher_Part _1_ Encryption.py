fea1e0184c1786d76f4b16b39d93bb5682667e97
RenataKamalova <rene.kamalova@gmail.com>
2022-10-03 17:30:19 +0300

    improved 85 lesson part 1 day 8

--- day 8/lesson_85/Caesar_Cipher_Part _1_ Encryption.py
+++ day 8/lesson_85/Caesar_Cipher_Part _1_ Encryption.py
@@ -33,42 +33,30 @@ alphabet = [
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-# print(len(alphabet), alphabet[21])
 
 
-def encrypt(plain_text, shift_amount):
+def caesar(plain_text, shift_amount, direction):
     ciphertext = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        if new_position <= len(alphabet) - 1:
-            new_letter = alphabet[new_position]
-            ciphertext += new_letter
-        else:
-            new_letter = alphabet[new_position - len(alphabet)]
-            ciphertext += new_letter
-
-    print(f"The encoded text is {ciphertext}")
-
-
-def decrypt(plain_text, shift_amount):
     deciphered_text = ""
+
     for letter in plain_text:
         position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        deciphered_text += new_letter
-        # if new_position > 0:
-        #     new_letter = alphabet[new_position]
-        #     deciphered_text += new_letter
-        # else:
-        #     new_letter = alphabet[new_position - 1]
-        #     deciphered_text += new_letter
 
-    print(f"The decoded text is {deciphered_text}")
+        if direction == "encode":
+            new_position = position + shift_amount
+            if new_position <= len(alphabet) - 1:
+                new_letter = alphabet[new_position]
+                ciphertext += new_letter
+            else:
+                new_letter = alphabet[new_position - len(alphabet)]
+                ciphertext += new_letter
+        print(f"The encoded text is {ciphered_text}")
+        
+        else:            
+            new_position = position - shift_amount
+            new_letter = alphabet[new_position]
+            deciphered_text += new_letter
 
+        print(f"The decoded text is {deciphered_text}")
 
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
+caesar()
