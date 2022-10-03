afa33fc4070bc6a7b7e28ae2e58ba25e704b7776
RenataKamalova <rene.kamalova@gmail.com>
2022-10-03 11:48:56 +0300

    implemented 81 lesson day 8

---
+++ day 8/lesson_81.py
@@ -0,0 +1,15 @@
+# functions with inputs
+
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+def greet():
+    print("Hello, dear friend!")
+    print("You are building a great function")
+    print("Good luck!")
+
+
+greet()
