1c75de75e3da8b26c61d7bff60992f6d650986d6
RenataKamalova <rene.kamalova@gmail.com>
2022-10-04 18:51:56 +0300

    implemented 93 lesson day 9

---
+++ day 9/lesson_93.py
@@ -0,0 +1,10 @@
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 9,
+    },
+}
+
+
+print(travel_log)
