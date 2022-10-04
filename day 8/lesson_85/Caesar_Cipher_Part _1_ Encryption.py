8603739460d6d3db0a37c6198b15516d0ca12a20
RenataKamalova <rene.kamalova@gmail.com>
2022-10-04 18:24:34 +0300

    improved 85 lesson part 1 day 8

--- day 8/lesson_85/Caesar_Cipher_Part _1_ Encryption.py
+++ day 8/lesson_85/Caesar_Cipher_Part _1_ Encryption.py
@@ -1,33 +1,10 @@
 from operator import index
+from tkinter.messagebox import QUESTION
+from art import logo
+from art import english_alphabet
 
-alphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-]
+print(logo)
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
@@ -36,25 +13,23 @@ shift = int(input("Type the shift number:\n"))
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     for letter in start_text:
-        position = alphabet.index(letter)
+        position = english_alphabet.index(letter)
         if cipher_direction == "decode":
             shift_amount *= -1
             new_position = position + shift_amount
-            new_letter = alphabet[new_position]
+            new_letter = english_alphabet[new_position]
             end_text += new_letter
             shift_amount *= -1
 
         elif cipher_direction == "encode":
             new_position = position + shift_amount
-            if new_position <= len(alphabet) - 1:
-                new_letter = alphabet[new_position]
+            if new_position <= len(english_alphabet) - 1:
+                new_letter = english_alphabet[new_position]
                 end_text += new_letter
             else:
-                new_letter = alphabet[new_position - len(alphabet)]
+                new_letter = english_alphabet[new_position - len(english_alphabet)]
                 end_text += new_letter
-
-        # end_text += alphabet[new_position]
-    print(f"The {cipher_direction}d text is {end_text}")
+        print(f"The {cipher_direction}d text is {end_text}")
 
 
 caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
