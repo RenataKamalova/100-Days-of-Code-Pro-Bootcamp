9863dabb23dce4b7ad9c9921e7b4e1fd746b7ef2
RenataKamalova <rene.kamalova@gmail.com>
2022-10-05 17:36:00 +0300

    implemented lesson_95 day 9

--- day 9/lesson_95/lesson_95.py
+++ day 9/lesson_95/lesson_95.py
@@ -4,24 +4,29 @@ print(logo)
 print("Welcome to the secret auction programm.")
 
 
-def auction_dict(participant, value):
-    auction = {
-        participant: value,
-    }
-    print(auction)
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
 
 
 continue_flag = True
+bids = {}
 
 while continue_flag == True:
 
     name = input("What is your name? ")
-    bid = int(input("What's your bid?: $"))
-    answer_to_question = input("Are there any other bidders? Y/N ")
+    price = int(input("What's your bid?: $"))
+    bids[name] = price
+    answer_to_question = input("Are there any other bidders? y/n ")
 
-    if answer_to_question == "Y":
+    if answer_to_question == "y":
         continue_flag = True
     else:
         continue_flag = False
-
-auction_dict(name, bid)
+        find_highest_bidder(bids)
