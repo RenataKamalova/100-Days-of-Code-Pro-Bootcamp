6fe081fe0a7d53b2bbb1e2728d96d5b5b95aa86a
RenataKamalova <rene.kamalova@gmail.com>
2022-10-06 21:21:52 +0300

    implemented return function lesson_99 day 10

--- day 10/lesson_99.py
+++ day 10/lesson_99.py
@@ -1,6 +1,7 @@
 def format_name(f_name, l_name):
-    print(f_name.title())
-    print(l_name.title())
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
 
 
-format_name("ginger", "meyer")
+print(format_name("ginger", "meyer"))
