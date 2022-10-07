6226fa55bb7ec3b595a6d2c2923f3292e72376de
RenataKamalova <rene.kamalova@gmail.com>
2022-10-07 19:33:08 +0300

    improved lesson_101 day 10

--- day 10/lesson_101.py
+++ day 10/lesson_101.py
@@ -13,14 +13,11 @@ def is_leap(year):
 
 def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if is_leap(year):
-        month_days[1] = 29
-        return month_days[month - 1]
-    else:
-        return month_days[month - 1]
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month - 1]
 
 
-# 🚨 Do NOT change any of the code below
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
