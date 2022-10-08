edd05c2e9b773b418b71750af459ade75ff44b11
RenataKamalova <rene.kamalova@gmail.com>
2022-10-08 19:53:52 +0300

    implemented lesson_105 day 10

--- day 10/lesson_105.py
+++ day 10/lesson_105.py
@@ -15,26 +15,35 @@ def divide(n1, n2):
 
 
 operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
-num_1 = int(input("What's the first number? "))
 
-for symbol in operations:
-    print(symbol)
 
-continue_flag = 1
+def calculator():
+    num_1 = int(input("What's the first number? "))
 
-while continue_flag == 1:
+    for symbol in operations:
+        print(symbol)
 
-    operation_symbol = input("Pick an operation: ")
-    num_2 = int(input("What's the next number? "))
+    continue_flag = 1
 
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(num_1, num_2)
-    print(f"{num_1} {operation_symbol} {num_2} = {answer}")
+    while continue_flag == 1:
 
-    if (
-        input(f"type 'y' to continue calculation with {answer} or type 'n' to exit ")
-        == "y"
-    ):
-        num_1 = answer
-    else:
-        continue_flag == 0
+        operation_symbol = input("Pick an operation: ")
+        num_2 = int(input("What's the next number? "))
+
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num_1, num_2)
+        print(f"{num_1} {operation_symbol} {num_2} = {answer}")
+
+        if (
+            input(
+                f"type 'y' to continue calculation with {answer} or type 'n' to start a new calculation "
+            )
+            == "y"
+        ):
+            num_1 = answer
+        else:
+            continue_flag == 0
+            calculator()
+
+
+calculator()
