66a757ec42062d5cf6f7c4507f7d8a53420c74d2
RenataKamalova <rene.kamalova@gmail.com>
2022-10-08 19:23:44 +0300

    improved lesson_105 day 10

---
+++ day 10/lesson_105.py
@@ -0,0 +1,46 @@
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
+num_1 = int(input("What's the first number? "))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the line above: ")
+num_2 = int(input("What's the second number? "))
+
+calculation_function = operations[operation_symbol]
+first_answer = calculation_function(num_1, num_2)
+print(f"{num_1} {operation_symbol} {num_2} = {first_answer}")
+
+continue_flag = 1
+
+answer_to_question = input("Continue? y/n ")
+
+
+while continue_flag == 1:
+    if answer_to_question == "y":
+        operation_symbol = input("Pick another operation?")
+        num_3 = int(input("What's the next number? "))
+
+        calculation_function = operations[operation_symbol]
+
+        second_answer = calculation_function(first_answer, num_3)
+
+        print(f"{first_answer} {operation_symbol} {num_3} = {second_answer}")
+    else:
+        continue_flag = 0
