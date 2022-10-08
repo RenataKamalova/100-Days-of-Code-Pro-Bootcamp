69eee8a9e2476eecd09e100155aefa9feaeeb5ed
RenataKamalova <rene.kamalova@gmail.com>
2022-10-08 13:45:03 +0300

    improved lesson_103 day 10

--- day 10/lesson_103.py
+++ day 10/lesson_103.py
@@ -16,12 +16,14 @@ def divide(n1, n2):
 
 operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
 num_1 = int(input("What's the first number? "))
-num_2 = int(input("What's the second number? "))
 
 for symbol in operations:
     print(symbol)
 
 operation_symbol = input("Pick an operation from the line above: ")
+
+num_2 = int(input("What's the second number? "))
+
 calculation_function = operations[operation_symbol]
 answer = calculation_function(num_1, num_2)
 
