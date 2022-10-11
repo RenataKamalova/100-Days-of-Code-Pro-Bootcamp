8d2eb6a2f823a729b92d557f9743e416df830e85
RenataKamalova <rene.kamalova@gmail.com>
2022-10-11 08:29:10 +0300

    improved lesson_120 added function day 12

--- day 12/lesson_120/lesson_120.py
+++ day 12/lesson_120/lesson_120.py
@@ -20,31 +20,29 @@ def compare_numbers(estimated_number, hidden_number):
         return f"Too high. Guess again."
 
 
+def level_difficulty(x):
+    if x == "easy":
+        attemps = 10
+    elif x == "hard":
+        attemps = 5
+    else:
+        print("Invalid input. Type 'hard' or 'easy'")
+
+    return attemps
+
+
 new_number = create_number()
 print(logo)
 print("Welcome to the number guessing game!")
 print("I'm thinking of a number between 1 to 100")
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-if difficulty == "easy":
-    for i in range(0, 10):
-        print(f"You have {10 - i} attempts remaining to guess the number.")
-        current_estimated_number = int(input("Make a guess: "))
-        print(compare_numbers(current_estimated_number, new_number))
-        if 10 - i == 1 and current_estimated_number != new_number:
-            print("You've run out of guesses, you lose.")
-        if current_estimated_number == new_number:
-            break
+n = level_difficulty(difficulty)
 
-
-elif difficulty == "hard":
-    for i in range(0, 5):
-        print(f"You have {5 - i} attempts remaining to guess the number.")
-        current_estimated_number = int(input("Make a guess: "))
-        print(compare_numbers(current_estimated_number, new_number))
-        if 5 - i == 1 and current_estimated_number != new_number:
-            print("You've run out of guesses, you lose.")
-        if current_estimated_number == new_number:
-            break
-
-else:
-    print("Invalid input. Type 'hard' or 'easy'")
+for i in range(0, n):
+    print(f"You have {n - i} attempts remaining to guess the number.")
+    current_estimated_number = int(input("Make a guess: "))
+    print(compare_numbers(current_estimated_number, new_number))
+    if n - i == 1 and current_estimated_number != new_number:
+        print("You've run out of guesses, you lose.")
+    if current_estimated_number == new_number:
+        break
