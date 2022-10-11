450eabd5e51ce4ea22e2f9151eedc9a9898b8ba2
RenataKamalova <rene.kamalova@gmail.com>
2022-10-11 09:27:04 +0300

    improved lesson_120 day 12

--- day 12/lesson_120/lesson_120.py
+++ day 12/lesson_120/lesson_120.py
@@ -21,22 +21,23 @@ def level_difficulty(x):
         return 0
 
 
-new_number = random.randint(1, 100)
+answer = random.randint(1, 100)
 print(logo)
 print("Welcome to the number guessing game!")
 print("I'm thinking of a number between 1 to 100")
 
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 n = level_difficulty(difficulty)
+
 if n != 5 and n != 10:
     print("Invalid input. Type 'hard' or 'easy'")
-print(f"число = {new_number}")
+print(f"число = {answer}")
 
 for i in range(0, n):
     print(f"You have {n - i} attempts remaining to guess the number.")
     current_estimated_number = int(input("Make a guess: "))
-    print(compare_numbers(current_estimated_number, new_number))
-    if n - i == 1 and current_estimated_number != new_number:
-        print("You've run out of guesses, you lose.")
-    if current_estimated_number == new_number:
+    print(compare_numbers(current_estimated_number, answer))
+    if n - i == 1 and current_estimated_number != answer:
+        print(f"You've run out of guesses, you lose. The answer was {answer}")
+    if current_estimated_number == answer:
         break
