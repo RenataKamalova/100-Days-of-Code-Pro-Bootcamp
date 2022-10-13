97751e62bdd749408be40f56ad67a5d25bea02ba
RenataKamalova <rene.kamalova@gmail.com>
2022-10-13 10:42:53 +0300

    implemened lesson_130 day 13

---
+++ day 13/lesson_130.py
@@ -0,0 +1,7 @@
+# number = int(input("Which number do you want to check?"))
+
+for i in range(0, 21):
+    if i % 2 == 0:
+        print(f"{i} - even number")
+    else:
+        print(f"{i} - odd number")
