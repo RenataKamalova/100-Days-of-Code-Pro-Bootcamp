9f21db8f5bb5140e707f986427ad871093b38fc4
RenataKamalova <rene.kamalova@gmail.com>
2022-10-14 20:46:36 +0300

    started solution from 134 lesson day 134

---
+++ day 14/lesson 134/solution.py
@@ -0,0 +1,15 @@
+import random
+
+countries_land_size = {}
+
+with open("countries.txt") as topo_file:
+    for line in topo_file:
+        [country, land_size] = line.split(",")
+        countries_land_size[country] = int(land_size)
+
+all_countries = list(countries_land_size.keys())
+country_1 = random.choice(all_countries)
+country_2 = random.choice(all_countries)
+
+print(f"{country_1} = {countries_land_size[country_1]}")
+print(f"{country_2} = {countries_land_size[country_2]}")
