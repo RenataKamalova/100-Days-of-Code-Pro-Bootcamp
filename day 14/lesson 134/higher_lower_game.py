b299d953db0fffb400d7c68e37538614cbc768f0
RenataKamalova <rene.kamalova@gmail.com>
2022-10-16 19:28:37 +0300

    added my_answer to the_game for lesson 134 day 14

--- day 14/lesson 134/higher_lower_game.py
+++ day 14/lesson 134/higher_lower_game.py
@@ -8,3 +8,4 @@ b = random.choice(data)
 print(
     f'{logo}\nCompare A: {a["name"]}, a {a["description"]}, from {a["country"]}\n{vs}\nAgainst B: {b["name"]}, a {b["description"]}, from {b["country"]}'
 )
+my_answer = input("Who has more followers? Type 'A' or 'B'")
