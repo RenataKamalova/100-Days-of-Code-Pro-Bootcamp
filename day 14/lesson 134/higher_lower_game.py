66f7e73ca0c4ee31cd71567b101013568b5cafce
RenataKamalova <rene.kamalova@gmail.com>
2022-10-16 19:25:18 +0300

    started higher_lower_game for lesson 134 day 14

---
+++ day 14/lesson 134/higher_lower_game.py
@@ -0,0 +1,10 @@
+from art import logo, vs
+from data import data
+import random
+
+a = random.choice(data)
+b = random.choice(data)
+
+print(
+    f'Compare A: {a["name"]}, a {a["description"]}, from {a["country"]}\n{vs}\nAgainst B: {b["name"]}, a {b["description"]}, from {b["country"]}'
+)
