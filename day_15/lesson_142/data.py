e961b4c5940af0e9729ed49f74da07d5a67f5b93
RenataKamalova <rene.kamalova@gmail.com>
2022-10-19 15:14:54 +0300

    added functions to coffee machine lesson 142

--- day_15/lesson_142/data.py
+++ day_15/lesson_142/data.py
@@ -1,3 +1,6 @@
+from tkinter import Menu
+
+
 MENU = {
     "espresso": {
         "ingredients": {
--- day_15/lesson_142/solution.py
+++ day_15/lesson_142/solution.py
@@ -1,25 +1,53 @@
 from data import MENU, resources
 
+list_coins = {"quarters": 25, "dimes": 10, "nickles": 5, "pennies": 1}
+coins = []
+
 
 def check_request(current_answer):
     if current_answer in MENU:
+        cost_of_drink = MENU[current_answer]["cost"]
         print("Please insert coins.")
-        input("How many quarters?: \nHow many dimes?: \nHow many nickles?: \nHow many pennies?: ")
-        if current_answer == "espresso":
+        for coin in list_coins:
+            coins.append(list_coins[coin] * int(input(f"How many {coin}? ")))
 
-    # elif current_answer == "latte":
-    #     print("Please insert coins.")
-    # elif current_answer == "cappuccino":
-    #     print("Please insert coins.")
+        sum_coins = sum(coins) / 100
+        coins_check(sum_coins, cost_of_drink, current_answer)
+
+        is_used_ingredients(current_answer)
     elif current_answer == "report":
-        check_resources(resources)
+        print(resources)
     else:
         print("error")
 
 
-def check_resources(current_condition):
-    return current_condition
+def coins_check(current_sum, sum_of_drink, current_drink):
+    if current_sum == sum_of_drink:
+        print(f"Here's your {current_drink}. Enjoy!")
+    elif current_sum > sum_of_drink:
+        print(f"Your charge is {current_sum - sum_of_drink}")
+        print(f"Here's your {current_drink}. Enjoy!")
+    else:
+        print("Sorry there is not enough money")
+        print(f"{current_drink} costs {sum_of_drink}$. You inserted {current_sum}$")
 
 
-answer_to_question = input("What whould you like? ")
+def is_used_ingredients(drink):
+    if drink == "espresso":
+        resources["water"] -= MENU["espresso"]["ingredients"]["water"]
+        resources["coffee"] -= MENU["espresso"]["ingredients"]["coffee"]
+    elif drink == "latte":
+        resources["water"] -= MENU["latte"]["ingredients"]["water"]
+        resources["coffee"] -= MENU["latte"]["ingredients"]["coffee"]
+        resources["milk"] -= MENU["latte"]["ingredients"]["milk"]
+    elif drink == "cappuccino":
+        resources["water"] -= MENU["cappuccino"]["ingredients"]["water"]
+        resources["coffee"] -= MENU["cappuccino"]["ingredients"]["coffee"]
+        resources["milk"] -= MENU["cappuccino"]["ingredients"]["milk"]
+    else:
+        print("error")
+
+
+answer_to_question = input("What whould you like? (espresso/latte/cappuccino)\n")
 check_request(answer_to_question)
+print(coins)
