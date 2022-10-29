1007340e99b327d8e65982d92e0a35556d3005ca
RenataKamalova <renekamalova@gmail.com>
2022-10-29 13:21:26 +0300

    implemented lesson 147 from day 16

--- day_16/lesson_147/solution.py
+++ day_16/lesson_147/solution.py
@@ -1,10 +1,11 @@
-from turtle import Turtle, Screen, begin_fill
+from turtle import Turtle, Screen
 
 timmy = Turtle()
 print(timmy)
 timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
 
 my_screen = Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
-timmy.color("red")
