a7b515bffb31770c4ffe71c183f7c9afb6c9352b
RenataKamalova <renekamalova@gmail.com>
2022-11-02 14:19:20 +0300

    implemented step 2 lesson 150 from day 16

--- day_16/lesson_150/oop_coffeemachine_start/main.py
+++ day_16/lesson_150/oop_coffeemachine_start/main.py
@@ -4,6 +4,21 @@ from money_machine import MoneyMachine
 
 my_machine = MoneyMachine()
 my_coffee_maker = CoffeeMaker()
+my_menu = Menu()
 
-my_machine.report()
-my_coffee_maker.report()
+is_on = True
+
+
+
+while is_on:
+    options = my_menu.get_items()
+    choice = input(f"what would you like? ({options})")
+
+    if choice == 'off':
+        is_on = False
+    elif choice == 'report':
+        my_machine.report()
+        my_coffee_maker.report()
+    else:
+        drink = my_menu.find_drink(choice)
+        print(my_coffee_maker.is_resource_sufficient(drink))
