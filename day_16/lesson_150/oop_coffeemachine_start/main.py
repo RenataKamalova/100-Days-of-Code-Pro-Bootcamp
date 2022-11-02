b21f13b2b9c9b63e12a2c48573665dc471c806bf
RenataKamalova <renekamalova@gmail.com>
2022-11-02 13:59:15 +0300

    started lesson 150 from day 16

---
+++ day_16/lesson_150/oop_coffeemachine_start/main.py
@@ -0,0 +1,9 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+my_machine = MoneyMachine()
+my_coffee_maker = CoffeeMaker()
+
+my_machine.report()
+my_coffee_maker.report()
