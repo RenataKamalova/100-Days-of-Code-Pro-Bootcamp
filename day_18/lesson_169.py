31f28b7f075427f0e44233676a4a01783b38673a
RenataKamalova <rene.kamalova@gmail.com>
2022-11-26 09:53:22 +0300

    implemented lesson 169 from 18 day

--- day_18/lesson_169.py
+++ day_18/lesson_169.py
@@ -1,12 +1,20 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 screen = Screen()
+colors = ["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy"]
 
-for i in range(3,11):
-    angle = 360 / i
-    for x in range(i):        
+def shape(sides_number):
+    angle = 360 / sides_number
+    for x in range(sides_number):        
         tim.forward(100)
         tim.right(angle)
+
+
+for i in range(3,11):
+    some_color = random.choice(colors)
+    tim.color(some_color)
+    shape(i)
 
 screen.exitonclick()
