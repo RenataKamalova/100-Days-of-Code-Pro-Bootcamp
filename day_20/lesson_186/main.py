355b32254a1e4f48825254f3f52200e7a9aa60c9
RenataKamalova <rene.kamalova@gmail.com>
2022-11-29 16:02:51 +0300

    implemented lesson 186 from 20 day

--- day_20/lesson_186/main.py
+++ day_20/lesson_186/main.py
@@ -10,15 +10,11 @@ screen.tracer(0)
 
 snake = Snake()
 
-
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for segm_num in range(len(segments) - 1,0,-1):
-        new_x = segments[segm_num - 1].xcor()
-        new_y = segments[segm_num - 1].ycor()
-        segments[segm_num].goto(new_x,new_y) 
 
-    segments[0].forward(20)
+    snake.move()
+    
--- day_20/lesson_186/snake.py
+++ day_20/lesson_186/snake.py
@@ -1,17 +1,26 @@
 from turtle import Screen, Turtle
 
+STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
+MOVE_DISTANCE = 20
+
 class Snake:
 
     def __init__(self):
-        pass
+        self.segments = []  
+        self.create_snake()     
 
-    starting_positions = [(0,0), (-20,0), (-40,0)]
+    
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)    
+            self.segments.append(new_segment)
 
-    segments = []
-
-    for position in starting_positions:
-        new_segment = Turtle(shape="square")
-        new_segment.color("white")
-        new_segment.penup()
-        new_segment.goto(position)    
-        segments.append(new_segment)
+    def move(self):
+        for segm_num in range(len(self.segments) - 1,0,-1):
+            new_x = self.segments[segm_num - 1].xcor()
+            new_y = self.segments[segm_num - 1].ycor()
+            self.segments[segm_num].goto(new_x,new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
