b30459786f243375e2345d00672b2d5e12851ab4
RenataKamalova <rene.kamalova@gmail.com>
2022-12-01 20:12:37 +0300

    implemented lesson 193 from 21 day

--- day_21/main.py
+++ day_21/main.py
@@ -26,11 +26,16 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+
+    #detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False        
+        scoreboard.game_over()
+        
         
         
--- day_21/scoreboard.py
+++ day_21/scoreboard.py
@@ -1,22 +1,29 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = "Courier", 22, "normal"
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         
-        self.count = 0
+        self.score = 0
         self.color("white")
         self.penup()
         self.goto(0,265)
+        self.hideturtle()
         self.update_scoreboard()
-        self.ht()
+        
 
     def update_scoreboard(self):
-        self.write(f'Score: {self.count}', False, align="center", font=("Courier", 22, "normal"))        
-        
+        self.write(f'Score: {self.score}', align=ALIGNMENT, font=FONT)        
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)  
+
     def increase_score(self):
-        self.count += 1
+        self.score += 1
         self.clear()
         self.update_scoreboard()
         
