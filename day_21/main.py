a3f3a94d319a55863cba78e430e89c9c7ce2eb13
RenataKamalova <rene.kamalova@gmail.com>
2022-12-02 16:37:34 +0300

    implemented lesson 195 from 21 day

--- day_21/main.py
+++ day_21/main.py
@@ -33,9 +33,15 @@ while game_is_on:
         scoreboard.increase_score()
 
     #detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 285 or snake.head.xcor() < -285 or snake.head.ycor() > 285 or snake.head.ycor() < -285:
         game_is_on = False        
         scoreboard.game_over()
+
+    #detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
