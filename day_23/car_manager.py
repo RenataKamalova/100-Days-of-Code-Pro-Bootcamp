260465fb621effbc55edcb01beb04d50ba5e893f
RenataKamalova <rene.kamalova@gmail.com>
2022-12-05 19:12:16 +0300

    reduced frequently in car_manager class

--- day_23/car_manager.py
+++ day_23/car_manager.py
@@ -11,13 +11,16 @@ class CarManager(Turtle):
         self.all_cars = []
         
     def create_cars(self):
-        new_car = Turtle("square")
-        new_car.shapesize(1,2)
-        new_car.penup()
-        new_car.color(random.choice(COLORS))
-        random_y = random.randint(-250,250)
-        new_car.goto(300,random_y)                  
-        self.all_cars.append(new_car) 
+        random_chance = random.randint(1,6)
+
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(1,2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250,250)
+            new_car.goto(300,random_y)                  
+            self.all_cars.append(new_car) 
 
     def move_cars(self):       
         for car in self.all_cars:
