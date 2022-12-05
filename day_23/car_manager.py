9dc58664b8f66ef63fdb1e57481817798d053f4c
RenataKamalova <rene.kamalova@gmail.com>
2022-12-05 16:19:39 +0300

    fixed car_manager class

--- day_23/car_manager.py
+++ day_23/car_manager.py
@@ -7,28 +7,22 @@ MOVE_INCREMENT = 10
 
 
 class CarManager(Turtle):
-    def __init__(self):
-        super().__init__()        
-        self.penup()
-        self.height = 300
-        self.all_squares = []
+    def __init__(self):       
+        self.all_cars = []
         
-           
-            
-    
-        for i in range(10):            
-            self.shape('square')
-            self.color(random.choice(COLORS))
-            self.all_squares.append(self) 
+    def create_cars(self):
+        new_car = Turtle("square")
+        new_car.shapesize(1,2)
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        random_y = random.randint(-250,250)
+        new_car.goto(300,random_y)                  
+        self.all_cars.append(new_car) 
 
-    def move(self):       
-        for square in self.all_squares:
-            x_cord = 250
-            y_cord = self.height - 50
-            square.setheading(180)
-            square.goto(x_cord,y_cord)
-                          
-            square.forward(10)
+    def move_cars(self):       
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)                         
+            
             
             
         
--- day_23/main.py
+++ day_23/main.py
@@ -19,7 +19,8 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)   
     screen.update()
+    car_manager.create_cars()
+    car_manager.move_cars()
 
 
 
-screen.exitonclick()
