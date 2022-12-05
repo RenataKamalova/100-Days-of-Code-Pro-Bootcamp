76eb4767b63726ba634e71c3e7e9922516f7ff27
RenataKamalova <rene.kamalova@gmail.com>
2022-12-05 13:26:58 +0300

    day 23 moving player

--- day_23/player.py
+++ day_23/player.py
@@ -1,7 +1,23 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+
+        self.shape('turtle')
+        self.setheading(90)
+        self.penup()
+        self.goto(0,-255)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(),new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(),new_y)
